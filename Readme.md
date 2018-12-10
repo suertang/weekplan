1189ca319ac7599eee49de7db686996748c448b7
Zhongji Tang <suertang@gmail.com>
2018-12-10 16:21:02 +0800

    del cache

---
+++ Readme.md
@@ -0,0 +1,21 @@
+README.md
+### 1.Requirements
+Python requirement:
+````
+    1.bottle
+    2.paste
+````
+JavaScript:
+````
+    1.Jquery
+````
+### 2.Usage:
+````
+pip install bottle
+pip install paste
+python bot.py
+````
+goto http://127.0.0.1:8080
+
+### 3.License
+MIT
---
+++ bot.py
@@ -0,0 +1,67 @@
+#/usr/bin/env python
+#coding=utf-8
+from bottle import route, run,static_file
+from bottle import request,view,response
+import bottle
+import os
+import sqlite3
+import json
+
+@route('/sql', method = 'POST')
+def savejson():
+    try:
+        js=request.POST.get('data')
+        conn = sqlite3.connect('ddd.db')
+        c = conn.cursor()
+        js=json.loads(js)
+        id=js['firstday']
+        count=js['count']
+        jso=json.dumps(js['tbs'])
+        c.execute("""REPLACE INTO TBarrange (Week_ID,count,json)
+        VALUES (?,?,?)""",(id,count,jso))
+        conn.commit()
+        conn.close()
+        return {"status":"Saved"}
+    except:
+        return {'status':"Error"}
+
+@route('/ajax', method = 'GET')
+def ajax():
+    '''
+    ddd
+    '''
+    week=request.GET.get('week')
+    conn = sqlite3.connect('ddd.db')
+    c = conn.cursor()
+    r=c.execute('SELECT * FROM TBarrange WHERE Week_ID=?',(week,))
+    rec=r.fetchone()
+    if(rec):
+        #rec=r.fetchone()
+        ret={}
+        ret['firstday']=rec[0]
+        ret['count']=rec[1]
+        ret['tbs']=json.loads(rec[2])
+    else:
+        ret = {'status':'notFound'}
+    conn.close()
+    response.content_type = 'application/json'
+    return json.dumps(ret)
+
+
+
+@route('/<name:re:images|css|js|icons>/<path:path>')
+def static_img(name,path):
+    return static_file(path,root="./"+name)
+
+@route('/')
+def index():
+    return static_file('week.html',root="./")
+@route('/favicon.ico')
+def fav():
+    return static_file('favicon.ico',root="./")
+
+
+application = bottle.default_app()
+from paste import httpserver
+httpserver.serve(application, host='0.0.0.0', port=8080)
+#run(host='0.0.0.0', port=8080, debug=True)
